6a6bf854d16e822ae0e65f8211cd5641a892eb9d
bradfordlauren <bradfordlauren@yahoo.com>
2016-10-21 15:10:21 -0400

    Add files via upload

---
+++ EchoProgram/EchoProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence.");
+            string echoPlease = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine(echoPlease);
+            } while (true);
+            
+        }
+    }
+}
